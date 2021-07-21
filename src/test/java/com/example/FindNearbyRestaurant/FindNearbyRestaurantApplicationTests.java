58415e4748db08ae08661ace06e6506a70cc7912
Attaporn Peungsook <aofattapon321@gmail.com>
2021-07-21 18:05:11 +0700

    Initial

---
+++ src/test/java/com/example/FindNearbyRestaurant/FindNearbyRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FindNearbyRestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindNearbyRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
